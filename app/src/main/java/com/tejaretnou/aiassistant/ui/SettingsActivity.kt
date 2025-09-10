ecbdce5137a70305a644a0fc96eceeefe1c6b62e
ghadirb <ghadir.baraty@gmail.com>
2025-09-10 19:13:04 +0330

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/tejaretnou/aiassistant/ui/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.tejaretnou.aiassistant.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tejaretnou.aiassistant.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        // TODO: تنظیمات واقعی (انتخاب زبان، بکاپ Drive و ...)
+    }
+}
